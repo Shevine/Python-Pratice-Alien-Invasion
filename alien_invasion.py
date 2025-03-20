34599f71a217f8964f254fd83c9ea6418da99d2d
Shevine <shevine@outlook.com>
2025-03-20 21:15:04 +0800

    feat: 添加飞船类和游戏设置类，并集成到主游戏逻辑中
    此次提交引入了飞船类（Ship）和游戏设置类（Settings），并将它们集成到主游戏逻辑中。飞船类负责管理飞船的图像和位置，而设置类用于存储游戏屏幕的宽度、高度和背景颜色等配置。这些改动为游戏的基础功能奠定了基础。

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
-
 import pygame
+from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -9,8 +11,15 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -19,6 +28,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
